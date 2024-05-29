26f10ed0f4413ac92d2c822c972356c15cb8cd9c
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2024-05-29 15:54:00 +0300

    Grayscale and color image creations were added.

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import numpy as np
+from PIL import Image
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
+
 
 class DataReader:
 
     def read_file(self, filename):
-        data = np.genfromtxt(filename, delimiter=';', dtype=None)  
-        print([len(line) for line in data])
-        print(len(data))     
+        data = np.genfromtxt(filename, delimiter=';', dtype=None)
+        # print([len(line) for line in data])
+        # print(len(data))
+        print(data[0])     
+        return data
+
+    def create_image(self, raw_data):
+        img = Image.fromarray(raw_data)
+        img.show()
+
+    def create_color_image(self, raw_data):
+        height = len(raw_data)
+        width = len(raw_data[0])
+        pixels = []
+        for line in raw_data:
+            for value in line:
+                red = (value >> 16) & 0xff
+                green = (value >> 8) & 0xff
+                blue = value & 0xff
+                pixels.append((red, green, blue))
+        img = Image.new('RGB', (width, height))
+        img.putdata(pixels)
+        # img = Image.fromarray(raw_data, mode='RGB')
+        img.show()
+
 
 def main():
-    FILENAME = 'atom.csv'
+    # FILENAME = './task/attached_data/for_main_task/atom.csv'
+    FILENAME = './task/attached_data/for_extra_task/atom_rgb.csv'
+    # FILENAME = './task/attached_data/for_main_task/beam.csv'
+    # FILENAME = './task/attached_data/for_main_task/big_pic-7680x4320.csv'
 
     dr = DataReader()
-    dr.read_file(FILENAME)
+    raw_data = dr.read_file(FILENAME)
+    # dr.create_image(raw_data)
+    dr.create_color_image(raw_data)
 
 if __name__ == '__main__':
     main()

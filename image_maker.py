6d6518992b5d57850763b0911783f20687cac66b
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2024-06-02 14:27:52 +0300

    Image creation performance was improved.

--- image_maker.py
+++ image_maker.py
@@ -17,21 +17,55 @@ class ImageMaker:
                 # )
                 df = pd.read_csv(filename, sep=';', header=None)
                 data = df.values
-                print(type(data))
         except FileNotFoundError:
             print(f'File not found: {filename}')
         except PermissionError:
             print(f'Permission denied: {filename}')
         except Exception as e:
             print(f'Error with reading file {filename}: {e}')
-        print('File was read.')
         return data
+
+    def read_file_new(self, filename: str, sep_: str) -> dict:
+        data = None
+        try:
+            with open(filename, 'r') as f:
+                df = pd.read_csv(filename, sep=sep_, header=None)
+                data = df.values
+        except FileNotFoundError:
+            print(f'File not found: {filename}')
+        except PermissionError:
+            print(f'Permission denied: {filename}')
+        except Exception as e:
+            print(f'Error with reading file {filename}: {e}')
+        return data
+
+    def apply_colormap(self, data, colormap):
+        height = len(data)
+        width = len(data[0])
+        pixels = []
+        for line in data:
+            for value in line:
+                pixels.append(tuple(colormap[value]))
+        image = Image.new('RGB', (width, height))
+        image.putdata(pixels)
+        return image
+
+    def apply_colormap_new(self, data, colormap):
+        image = Image.fromarray(data.astype(np.uint8), mode='P')
+        palette = list(colormap.flat)
+        image.putpalette(palette)
+        return image
+
 
     def create_grayscale_image(self, data: np.ndarray) -> PilImage:
         height = len(data)
         width = len(data[0])
         image = Image.new('L', (width, height))
         image.putdata(list(data.flat))
+        return image
+
+    def create_grayscale_image_new(self, data: np.ndarray) -> PilImage:
+        image = Image.fromarray(data.astype(np.uint8), mode='L')
         return image
 
     def create_color_image(self, data: np.ndarray) -> PilImage:
@@ -87,15 +121,25 @@ def main():
     # FILENAME = './task/attached_data/for_main_task/beam.csv'
     FILENAME = './task/attached_data/for_main_task/big_pic-7680x4320.csv'
 
+    COLORMAP_FILENAME = './task/attached_data/colormap/CET-R1.csv'
+
     image_maker = ImageMaker()
     raw_data = image_maker.read_file(FILENAME)
-    # image = image_maker.create_grayscale_image(raw_data)
+    raw_colormap_data = image_maker.read_file_new(COLORMAP_FILENAME, ',')
+    print(raw_colormap_data[0])
+    # color_image = image_maker.apply_colormap_new(raw_data, raw_colormap_data)
+    # image_maker.save_image(color_image, 'colormap_beam.png')
+
+    image = image_maker.create_grayscale_image_new(raw_data)
     # image = image_maker.create_color_image(raw_data)
-    image = image_maker.create_image(raw_data, 'L')
+    # image = image_maker.create_image(raw_data, 'L')
 
     image_maker.save_image(image, 'grayscale_test_big.png')
     # image.show()
     print(raw_data[0])
+    # print(raw_colormap_data[0])
+    # print(len(raw_colormap_data[0]))
+    # print(type(raw_colormap_data[0]))
 
 
 if __name__ == '__main__':

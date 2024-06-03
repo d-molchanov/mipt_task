60c8d0173d6c8eba478b7aeeca652f4fe2dda37d
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2024-06-03 22:20:34 +0300

    Unused code was removed.

--- child_window_design.py
+++ child_window_design.py
@@ -24,35 +24,53 @@ class ChildWindowDesign(object):
         self.init_gui()
 
     def init_gui(self):
-        self.label = QLabel(self, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.area = QScrollArea()
-        self.area.setWidgetResizable(True)
-        self.button = QPushButton('Save as')
-        self.plus_button = QPushButton('+')
-        self.plus_button.setFixedSize(QSize(24, 24))
-        self.minus_button = QPushButton('-')
-        self.minus_button.setFixedSize(QSize(24, 24))
 
-        self.scale_edit = QLineEdit(self)
+        self.save_button = QPushButton('Save as', self)
+        self.decrease_scale_button = QPushButton('-', self)
+        self.decrease_scale_button.setFixedSize(24, 24)
+        self.scale_edit = QLineEdit(
+            self, alignment=Qt.AlignmentFlag.AlignRight
+        )
         self.scale_edit.setFixedWidth(42)
-        self.persentage = QLabel(self, text='%')
+        self.increase_scale_button = QPushButton('+', self)
+        self.increase_scale_button.setFixedSize(QSize(24, 24))
+        self.load_colormap_button = QPushButton('Load colormap', self)
+        self.load_colormap_button.setVisible(False)
+
+        self.interval_widget = QWidget(self)
+        interval_hbox = QHBoxLayout(self.interval_widget)
+        self.interval_edit = QLineEdit(
+            self, alignment=Qt.AlignmentFlag.AlignRight
+        )
+        self.interval_edit.setFixedWidth(42)
+        interval_hbox.addWidget(QLabel('Slideshow interval:', self))
+        interval_hbox.addWidget(self.interval_edit)
+        interval_hbox.addWidget(QLabel('s', self))
+        self.interval_widget.setVisible(False)
 
         self.hbox = QHBoxLayout()
-        self.hbox.addWidget(self.button)
-        self.hbox.addWidget(self.minus_button)
+        self.hbox.addWidget(self.save_button)
+        self.hbox.addWidget(self.decrease_scale_button)
         self.hbox.addWidget(self.scale_edit)
-        self.hbox.addWidget(self.persentage)
-        self.hbox.addWidget(self.plus_button)
+        self.hbox.addWidget(QLabel(self, text='%'))
+        self.hbox.addWidget(self.increase_scale_button)
+        self.hbox.addWidget(self.load_colormap_button)
+        self.hbox.addWidget(self.interval_widget)
         self.hbox.addStretch(1)
-        
+
+        self.image_label = QLabel(
+            self, alignment=Qt.AlignmentFlag.AlignCenter
+        )
+        self.image_scroll_area = QScrollArea(self)
+        self.image_scroll_area.setWidgetResizable(True)
+
         layout = QVBoxLayout()
         layout.addLayout(self.hbox)
-        layout.addWidget(self.area)
-        layout.addWidget(self.label)
+        layout.addWidget(self.image_scroll_area)
+        layout.addWidget(self.image_label)
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
-
         
         # self.show()
--- main_window.py
+++ main_window.py
@@ -13,8 +13,6 @@ class MainWindow(QMainWindow, MainWindowDesign):
 
         self.child_windows = []
         self.open_button.clicked.connect(self.show_open_dialog)
-        self.slideshow_checkbox.clicked.connect(self.test)
-        self.show()
 
     def create_window(self):
         child_window = ChildWindow()
@@ -22,35 +20,22 @@ class MainWindow(QMainWindow, MainWindowDesign):
 
     def show_open_dialog(self):
         filenames = QFileDialog.getOpenFileNames(self, 'Open file', '.')[0]
-        for f in filenames:
+        if self.slideshow_checkbox.isChecked():
             child_window = ChildWindow()
-            child_window.load_image(f)
+            child_window.show_slideshow(filenames)
             child_window.show()
             self.child_windows.append(child_window)
+        else:
+            for f in filenames:
+                child_window = ChildWindow()
+                child_window.load_image(f)
+                child_window.show()
+                self.child_windows.append(child_window)
 
-    def test(self):
-        print(self.slideshow_checkbox.isChecked())
-        # if self.slideshow_checkbox.isChecked():
-        #     self.period_hbox = QHBoxLayout()
-        #     self.led = QLineEdit(self)
-        #     self.led.setText('1')
-
-        #     self.lbl = QLabel(self)
-        #     self.lbl.setText('s')
-        #     self.period_hbox.addWidget(self.led)
-        #     self.period_hbox.addWidget(self.lbl)
-        #     self.period_hbox.addStretch(1)
-        #     self.vbox.insertLayout(1, self.period_hbox)
-
-        #     # self.vbox.addWidget(self.led)
-        #     # self.vbox.addStretch(1)
-        # else:
-        #     self.period_hbox.deleteLater()
-        #     self.led.deleteLater()
-        #     self.lbl.deleteLater()
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     main_window = MainWindow()
+    main_window.show()
     sys.exit(app.exec())
